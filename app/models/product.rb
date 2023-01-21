9d03cffc9b7191862274cd8715d79d3838b6b5a9
Felix Ondullah <felixondullah@gmail.com>
2023-01-21 08:31:12 +0300

    added realtionship

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,10 @@
 class Product < ApplicationRecord
+    class Product < ApplicationRecord
+        belongs_to :product_search
+    
+        def self.search(search_term)
+            Product.joins(:product_search).where("product_searches.search_term = ?", search_term)
+        end
+    end
+    
 end
