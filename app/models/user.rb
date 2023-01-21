ba15fc00bdf5604fe5b50dfdb06ea7c8ca539df4
Felix Ondullah <felixondullah@gmail.com>
2023-01-21 08:32:18 +0300

    added validation and relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+    has_many :product_searches, through: :user_searches
+    has_many :user_searches
+    has_secure_password
+    validates :username, uniqueness: { case_sensitive: false }
+    validates :password, confirmation: true, presence: true, length: {in: 6..20, messsage: "Password must be between 6 and 20 characters."}
+    validates :password_confirmation, presence: true
+
 end
