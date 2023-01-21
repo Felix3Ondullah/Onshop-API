d07d9a7681c9c55f9a5d2106889f0fe7d70050ee
Felix Ondullah <felixondullah@gmail.com>
2023-01-21 08:29:38 +0300

    added realtionship

--- app/models/product_search.rb
+++ app/models/product_search.rb
@@ -1,2 +1,11 @@
 class ProductSearch < ApplicationRecord
+    has_many :products
+    has_many :user_searches
+    attr_accessor :product_search_id
+
+    validates :search_term, presence: true
+
+    def self.search_history(user_id)
+        ProductSearch.joins(:user_searches).where(user_searches: {user_id: user_id})
+      end
 end
